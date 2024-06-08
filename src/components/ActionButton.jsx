8c6b6dd725ee7f14e5421389dd05127f37266e87
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-06-08 16:43:53 +0530

    minor updates

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -1,10 +1,10 @@
 import "../assets/css/ActionButton.css";
 import { IoChevronForward } from "react-icons/io5";
 
-export default function ActionButton({ link, innerText }) {
+export default function ActionButton({ link, innerText, btnColor="btn-clr-white" }) {
   return (
     <a href={link}>
-      <button className="actionBtn btn btn-light rounded-pill px-3 d-flex align-items-center gap-1">
+      <button className={`actionBtn btn btn-light rounded-pill px-3 d-flex align-items-center gap-1 ${btnColor}`}>
         {innerText}
         <IoChevronForward />
       </button>
