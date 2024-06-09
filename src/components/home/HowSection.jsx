0f1d5d356d5655ee54a8d64cb48e029e012e20cd
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-06-09 11:54:55 +0530

    minor updates

--- src/components/home/HowSection.jsx
+++ src/components/home/HowSection.jsx
@@ -1,9 +1,17 @@
 export default function HowSection() {
+    function handleOnScroll(e) {
+        console.log(e);
+        console.log("hhh");
+    }
     return (
         <div className="how-section d-flex flex-column align-items-center">
             <span className="rounded-pill p-2 px-4">How it works</span>
-            <h1 className="fs-2 fw-bolder pb-5">Onboard talent blazing fast</h1>
-            
+            <h1 className="fs-1 fw-bolder pb-5">Onboard talent blazing fast</h1>
+            <div className="steps w-100" onScrollCapture={handleOnScroll}>
+                <div className="step_line">
+                    <div className="step_line_inner w-100"></div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/home/ProjectSection.jsx
+++ src/components/home/ProjectSection.jsx
@@ -5,10 +5,10 @@ export default function ProjectSectiom() {
     return (
         <section className="projects w-100 d-flex flex-column align-items-center">
             <div className="projects-heading text-center px-md-0 px-4">
-                <h3 className="fw-bolder">A taste of what our talent can do for you</h3>
+                <h2 className="fw-bolder">A taste of what our talent can do for you</h2>
                 <p>This is worth a thousand words about us.</p>
             </div>
-            <div className="d-flex justify-content-center flex-wrap gap-4 w-100 px-md-0 px-4">
+            <div className="d-flex justify-content-center flex-wrap gap-5 w-100 px-md-0 px-4">
                 {projects.map((project, idx)=>(
                     <Projects link={project.link} src={project.src} key={idx}/>
                 ))}
