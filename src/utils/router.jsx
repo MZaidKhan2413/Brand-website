2c5b352b2c462d08f9271cd9c6fd6dafda6455fb
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-06-12 11:12:09 +0530

    About page added

--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import Home from "../components/home/Home.jsx"
+import Home from "../components/home/Home.jsx";
+import About from "../components/About/About.jsx";
 import Contact from "../components/Contact/Contact";
 import Error from "../components/Error/Error.jsx";
 
@@ -16,6 +17,10 @@ export const router = createBrowserRouter([
             {
                 path: "contact",
                 element: <Contact />
+            },
+            {
+                path: "about",
+                element: <About />
             },
             {
                 path: "*",
